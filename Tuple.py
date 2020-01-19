8e2d06f45534bf9b1f25af55ef70229f2cd2d6e1
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-01-20 01:56:08 +0600

    Add files via upload
    Functionalities of TUPLE

---
+++ Tuple.py
@@ -0,0 +1,15 @@
+# Tuples basic functionality is as same as a LIST.
+# The main diiference is TUPLE is immutable. Which means it can't be modified or changed.
+# Once a tuple is created, it's created forever.
+# Tuples initialized with parenthesis "()"
+
+
+# Initializing a TUPLE
+coordinates = (7,5)
+print(coordinates)
+print(coordinates[0]) # prints 7 as it's in index 0
+
+# LIST of TUPLEs
+name_id = [("Jodu", 12), ("Kodu", 17), ("Ludu", 23)]
+print(name_id) # prints the list of tuples
+print(name_id[0]) # prints ('Jodu', 12)
