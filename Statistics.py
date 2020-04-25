a93c5744d0187e273682af642b9c53aeaeb0e3d8
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-04-25 15:49:04 +0600

    Create Statistics.py

---
+++ Statistics.py
@@ -0,0 +1,10 @@
+from statistics import mean, median, mode, variance
+
+print(mean([10, 20, 30, 40, 50, 60])) # Average 35
+
+print(median([1, 2, 3, 4, 5])) # Middle Value 3
+print(median([1, 2, 3, 4, 5, 6])) # Middle value 3.5 (average of two values in case of even counter of numbers
+
+print(mode([1, 2, 3, 1, 1])) # mostfrequent value (1)
+
+print(variance([1, 2, 3, 4, 5])) # Variance in change 2.5
