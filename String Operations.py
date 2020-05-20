c081f16ce143f19dcc2fc16e3e43e176fdb20add
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-05-20 06:06:02 +0600

    Update String Operations.py

--- String Operations.py
+++ String Operations.py
@@ -25,3 +25,29 @@ print(message)  # bonjour and welcome to Paris!
 # str.swapcase() converts upper case to lower case and vice versa.
 
 # str.capitalize() changes the first character of the string to the title case and the rest to the lower case.
+
+whitespace_string = "     hey      "
+normal_string = "incomprehensibilities"
+ 
+# delete spaces from the left side
+whitespace_string.lstrip()  # "hey      "
+ 
+# delete "i" or "s" or "is" from the left side
+normal_string.lstrip("is")  # "ncomprehensibilities"
+ 
+# delete spaces from the right side
+whitespace_string.rstrip()  # "     hey"
+ 
+# delete "i" or "s" or "is" from the right side
+normal_string.rstrip("is")  # "incomprehensibilitie"
+ 
+# no spaces from both sides
+whitespace_string.strip()  # "hey"
+ 
+# delete trailing "i" or "s" or "is" from both sides
+normal_string.strip("is")  # "ncomprehensibilitie"
+
+word = "Mississippi"
+print(word.lstrip("ips"))  # "Mississippi"
+print(word.rstrip("ips"))  # "M"
+print(word.strip("Mips"))  # ""
