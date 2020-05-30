d90f863b6bdcbd127853e303168bb3b651017e5e
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-05-30 21:22:33 +0600

    Create String format method.py

---
+++ String format method.py
@@ -0,0 +1,10 @@
+print('Mix {}, {} and a {} to make an ideal omelet.'.format('2 eggs', '30 g of milk', 'pinch of salt'))
+# Mix 2 eggs, 30 g of milk and a pinch of salt to make an ideal omelet.
+
+
+print('{0} in the {1} by Frank Sinatra'.format('Strangers', 'Night'))
+# Strangers in the Night by Frank Sinatra
+
+
+print('{1} in the {0} by Frank Sinatra'.format('Strangers', 'Night'))
+# Night in the Strangers by Frank Sinatra
