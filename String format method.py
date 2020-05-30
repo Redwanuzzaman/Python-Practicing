3ba8c1e8441c5c905938f01834630bbe24dd2268
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-05-30 21:41:23 +0600

    Update String format method.py

--- String format method.py
+++ String format method.py
@@ -8,3 +8,17 @@ print('{0} in the {1} by Frank Sinatra'.format('Strangers', 'Night'))
 
 print('{1} in the {0} by Frank Sinatra'.format('Strangers', 'Night'))
 # Night in the Strangers by Frank Sinatra
+
+
+print('The {film} at {theatre} was {adjective}!'.format(film='Lord of the Rings',
+                                                        adjective='incredible',
+                                                        theatre='BFI IMAX'))
+# The Lord of the Rings at BFI IMAX was incredible!
+
+
+print('The {0} was {adjective}!'.format('Lord of the Rings', adjective='incredible'))
+# The Lord of the Rings was incredible!
+
+
+print('The {0} was {adjective}!'.format(adjective='incredible', 'Lord of the Rings'))
+# SyntaxError: positional argument follows keyword argument
