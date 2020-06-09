7f28b2c7b5022be97579654550c7ef28ccef704d
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-06-09 23:58:48 +0600

    Update List Comprehension.py

--- List Comprehension.py
+++ List Comprehension.py
@@ -8,3 +8,11 @@ greater_than_5 = [x for x in numbers if x > 5]
 # printing your results
 print(less_than_5)
 print(greater_than_5)
+
+
+
+# Write a program that takes a list with words, creates a new list of words that start with the letter "a" in the first list, and prints it.
+
+# work with the preset variable `words`
+ans = [x for x in words if x.startswith('a') or x.startswith('A')]
+print(ans)
