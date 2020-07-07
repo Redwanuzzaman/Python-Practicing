37aa18d5f4170b23c7a096645332bc069f27121d
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-07-07 23:49:35 +0600

    Extracting Data from JSON

--- Web Scraping/Extracting Data from JSON.py
+++ Web Scraping/Extracting Data from JSON.py
@@ -3,6 +3,8 @@ import json
 
 address = input('Enter location: ')
 data = urlopen(address).read().decode()
+
+# data collected from web represents as a JSON string
 json_data = json.loads(data)
 
 ans = 0
