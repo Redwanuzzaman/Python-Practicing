71b27c7f0f204d1184e51f77efc31b1367901e62
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-09-23 14:08:19 +0600

    Exception Handling

--- Exception Handling.py
+++ Exception Handling.py
@@ -44,3 +44,53 @@ if not type(x) is int:
   raise TypeError("Only integers are allowed")
   
   
+# CATCHING EXCEPTIONS
+
+# import module sys to get the type of exception
+import sys
+
+randomList = ['a', 0, 2]
+
+for entry in randomList:
+    try:
+        print("The entry is", entry)
+        r = 1/int(entry)
+        break
+    except:
+        print("Oops!", sys.exc_info()[0], "occurred.")
+        print("Next entry.")
+        print()
+print("The reciprocal of", entry, "is", r)
+
+# output:
+# The entry is a
+# Oops! <class 'ValueError'> occurred.
+# Next entry.
+# 
+# The entry is 0
+# Oops! <class 'ZeroDivisionError'> occured.
+# Next entry.
+# 
+# The entry is 2
+# The reciprocal of 2 is 0.5# 
+
+
+randomList = ['a', 0, 2]
+
+for entry in randomList:
+    try:
+        print("The entry is", entry)
+        r = 1/int(entry)
+        break
+    except Exception as e:
+        print("Oops!", e.__class__, "occurred.")
+        print("Next entry.")
+        print()
+print("The reciprocal of", entry, "is", r)
+
+# output: SAME
+
+
+except (TypeError, ZeroDivisionError):
+   # handle multiple exceptions
+   # TypeError and ZeroDivisionError
