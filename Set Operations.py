e2923e963ddb5ca68ae99774a82f65f45869c8dd
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-09-26 22:13:04 +0600

    Update Set Operations.py

--- Set Operations.py
+++ Set Operations.py
@@ -1,18 +1,153 @@
-s = set("Hacker")
-print s.union("Rank")
-# set(['a', 'R', 'c', 'r', 'e', 'H', 'k', 'n'])
+SET: 
+A set is an unordered collection of items. Every set element is unique (no duplicates) and must be immutable (cannot be changed).
+However, a set itself is mutable. We can add or remove items from it.
+Sets can also be used to perform mathematical set operations like union, intersection, symmetric difference, etc.
 
-print s.union(set(['R', 'a', 'n', 'k']))
-# set(['a', 'R', 'c', 'r', 'e', 'H', 'k', 'n'])
+Creating Python Sets
+A set is created by placing all the items (elements) inside curly braces {}, separated by comma, or by using the built-in set() function.
+It can have any number of items and they may be of different types (integer, float, tuple, string etc.). But a set cannot have mutable elements like lists, sets or dictionaries as its elements.
+# Different types of sets in Python
+# set of integers
+my_set = {1, 2, 3}
+print(my_set)
 
-print s.union(['R', 'a', 'n', 'k'])
-# set(['a', 'R', 'c', 'r', 'e', 'H', 'k', 'n'])
+# set of mixed datatypes
+my_set = {1.0, "Hello", (1, 2, 3)}
+print(my_set)
+Output
+{1, 2, 3}
+{1.0, (1, 2, 3), 'Hello'}
+Try the following examples as well.
+# set cannot have duplicates
+# Output: {1, 2, 3, 4}
+my_set = {1, 2, 3, 4, 3, 2}
+print(my_set)
 
-print s.union(enumerate(['R', 'a', 'n', 'k']))
-# set(['a', 'c', 'r', 'e', (1, 'a'), (2, 'n'), 'H', 'k', (3, 'k'), (0, 'R')])
+# we can make set from a list
+# Output: {1, 2, 3}
+my_set = set([1, 2, 3, 2])
+print(my_set)
 
-print s.union({"Rank":1})
-# set(['a', 'c', 'r', 'e', 'H', 'k', 'Rank'])
+# set cannot have mutable items
+# here [3, 4] is a mutable list
+# this will cause an error.
 
-s | set("Rank")
-# set(['a', 'R', 'c', 'r', 'e', 'H', 'k', 'n'])
+my_set = {1, 2, [3, 4]}
+Output
+{1, 2, 3, 4}
+{1, 2, 3}
+Traceback (most recent call last):
+  File "<string>", line 15, in <module>
+    my_set = {1, 2, [3, 4]}
+TypeError: unhashable type: 'list'
+
+Modifying a set in Python
+Sets are mutable. However, since they are unordered, indexing has no meaning.
+We cannot access or change an element of a set using indexing or slicing. Set data type does not support it.
+We can add a single element using the add() method, and multiple elements using the update() method. The update() method can take tuples, lists, strings or other sets as its argument. In all cases, duplicates are avoided.
+# initialize my_set
+my_set = {1, 3}
+print(my_set)
+
+# if you uncomment line 9,
+# you will get an error
+# TypeError: 'set' object does not support indexing
+
+# my_set[0]
+
+# add an element
+# Output: {1, 2, 3}
+my_set.add(2)
+print(my_set)
+
+# add multiple elements
+# Output: {1, 2, 3, 4}
+my_set.update([2, 3, 4])
+print(my_set)
+
+# add list and set
+# Output: {1, 2, 3, 4, 5, 6, 8}
+my_set.update([4, 5], {1, 6, 8})
+print(my_set)
+Removing elements from a set
+A particular item can be removed from a set using the methods discard() and remove().
+The only difference between the two is that the discard() function leaves a set unchanged if the element is not present in the set. On the other hand, the remove() function will raise an error in such a condition (if element is not present in the set).
+The following example will illustrate this.
+# Difference between discard() and remove()
+
+# initialize my_set
+my_set = {1, 3, 4, 5, 6}
+print(my_set)
+
+# discard an element
+# Output: {1, 3, 5, 6}
+my_set.discard(4)
+print(my_set)
+
+# remove an element
+# Output: {1, 3, 5}
+my_set.remove(6)
+print(my_set)
+
+# discard an element
+# not present in my_set
+# Output: {1, 3, 5}
+my_set.discard(2)
+print(my_set)
+
+# remove an element
+# not present in my_set
+# you will get an error.
+# Output: KeyError
+
+my_set.remove(2)
+Traceback (most recent call last):
+  File "<string>", line 28, in <module>
+KeyError: 2
+
+PYTHON SET OPERATIONS:
+
+# Set union method
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use | operator
+# Output: {1, 2, 3, 4, 5, 6, 7, 8}
+print(A | B)
+# use union function
+>>> A.union(B)
+{1, 2, 3, 4, 5, 6, 7, 8}
+# Intersection of sets
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use & operator
+# Output: {4, 5}
+print(A & B)
+# use intersection function on A
+>>> A.intersection(B)
+{4, 5}
+# Difference of two sets
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use - operator on A
+# Output: {1, 2, 3}
+print(A - B)
+# use difference function on A
+>>> A.difference(B)
+{1, 2, 3}
+# Symmetric difference of two sets
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use ^ operator
+# Output: {1, 2, 3, 6, 7, 8}
+print(A ^ B)
+use symmetric_difference function on A
+>>> A.symmetric_difference(B)
+{1, 2, 3, 6, 7, 8}
